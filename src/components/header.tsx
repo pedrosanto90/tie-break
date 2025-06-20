b9a552d24040b6b731876a9f68aaaf4431322dab
pedrosanto90 <pedro.bb.90@gmail.com>
2025-06-20 15:45:48 +0100

    remove a tags and added Link tags

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link"
+
 export default function Header() {
   return (
     <div className="flex flex-col items-center justify-center p-5 bg-blue-500">
-      <h1 className="text-2xl"><a href="/">Tie Break 🎾 🥎</a></h1>
+      <h1 className="text-2xl"><Link href="/">Tie Break 🎾 🥎</Link></h1>
       <p className="mt-2">All about your padel games</p>
     </div>
   )
